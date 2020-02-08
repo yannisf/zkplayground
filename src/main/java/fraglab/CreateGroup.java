8a7484c7b2b92ff7e300e6b06e94eeafc9ae4601
yannisf <yannisf@ouranos>
2020-02-08 10:20:52 +0200

    Initial commit

---
+++ src/main/java/fraglab/CreateGroup.java
@@ -0,0 +1,43 @@
+package fraglab;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class CreateGroup implements Watcher {
+
+    private static final int SESSION_TIMEOUT = 5000;
+    private ZooKeeper zk;
+    private CountDownLatch connectedSignal = new CountDownLatch(1);
+
+    public void connect(String hosts) throws IOException, InterruptedException {
+        zk = new ZooKeeper(hosts, SESSION_TIMEOUT, this);
+        connectedSignal.await();
+    }
+
+    @Override
+    public void process(WatchedEvent event) { // Watcher interface
+        if (event.getState() == Event.KeeperState.SyncConnected) {
+            connectedSignal.countDown();
+        }
+    }
+
+    public void create(String groupName) throws KeeperException, InterruptedException {
+        String path = "/" + groupName;
+        String createdPath = zk.create(path, null/*data*/, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println("Created " + createdPath);
+    }
+
+    public void close() throws InterruptedException {
+        zk.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        CreateGroup createGroup = new CreateGroup();
+        createGroup.connect(Values.SERVER);
+        createGroup.create(Values.GROUP1 + "/" + Values.GROUP2);
+        createGroup.close();
+    }
+
+}
---
+++ src/main/java/fraglab/Values.java
@@ -0,0 +1,10 @@
+package fraglab;
+
+public class Values {
+
+    public static String SERVER = "192.168.1.14:2181";
+    public static String GROUP1 = "my_group1";
+    public static String GROUP2 = "my_group2";
+    public static String NODE1 = "my_node1";
+
+}
